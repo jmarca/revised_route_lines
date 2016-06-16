aa70b11223c0fb48c2c77b499f0a8d20f873ed1c
James E. Marca <james@activimetrics.com>
2016-06-15 23:09:57 -0700

    older tweak to test, didn't commit
    perhaps it was broken?  whatever, looks good to me

--- test/split_lines.sql
+++ test/split_lines.sql
@@ -8,21 +8,22 @@ SELECT no_plan();
 
 SELECT pass('Test split_lines!');
 
+PREPARE tallyup AS
+   SELECT count(*)::integer
+   FROM tempseg.route_relations_split_lines;
 
-SELECT is(
-    (SELECT count(*)
-          FROM tempseg.route_relations_split_lines
-    )::integer,
-    (select CASE
+PREPARE this_or_that AS
+   SELECT CASE
             WHEN exists(SELECT tablename
                         FROM pg_tables
                         WHERE schemaname='tempseg'
                           AND tablename='fixed_relations')
             THEN 1884
             ELSE 2799
-            END),
-    'The data got loaded okay'
-);
+            END;
+
+
+SELECT results_eq( 'tallyup', 'this_or_that', 'The data got loaded okay');
 
 SELECT finish();
 ROLLBACK;
